88a993fcc138edc6d5e9cd3c9fe66a7a2f43dc14
zeyuli <lizeyu1101@gmail.com>
2022-01-05 01:24:15 +0100

    complete first part of data preperation.

--- lama/preprocessing/DataProcessor.py
+++ lama/preprocessing/DataProcessor.py
@@ -1,11 +1,37 @@
+import typing as t
+import numpy as np
+import matplotlib.pyplot as plt
 import pandas as pd
+from pandas.core.common import SettingWithCopyWarning
 
+from lama.util.decorators import suppress_warning
 
-def nans(df: pd.DataFrame):
+def nans(df: pd.DataFrame) -> int:
     return df.isnull().sum()
 
 
-def change_object_cols(se: pd.DataFrame):
+def change_object_cols(se: pd.DataFrame, rule=lambda value: range(len(value))) -> np.ndarray:
     value = se.unique().tolist()
     value.sort()
-    return se.map(pd.Series(range(len(value)), index=value)).values
+    return se.map(pd.Series(rule(value), index=value)).values
+
+
+@suppress_warning(warning=SettingWithCopyWarning)
+def reformat_dataframe(df: pd.DataFrame, features: t.List[str], mapper) -> pd.DataFrame:
+    """
+    reformat the dataframe column by column with mapper, the function will replace 
+    the given feature columns by slice, as python doesn't support pointer.
+
+
+    if you would like to define a mapper function with more parameters, you can either define a 
+    function using lambda or use functools.partial or functools.partialmethod
+
+    for example:
+        to define a change-object_cols function with self implemented rule
+        mapper = functools.partial(change_object_cols, rule=${your_rule})
+    """
+    for feature in features:
+        se_mapper = mapper(df[feature])
+        df.loc[:, feature] = se_mapper
+    return df
+
--- lama/util/decorators.py
+++ lama/util/decorators.py
@@ -1,32 +1,31 @@
 import functools
-import logging
-import sys
 from datetime import time
-
-
-def debug(func, stream=sys.stdout):
-    """Print the function signature and return value"""
-    @functools.wraps(func)
-    def wrapper_debug(*args, **kwargs):
-        logging.basicConfig(stream=stream, level=logging.DEBUG)
-        args_repr = [repr(a) for a in args]                      # 1
-        kwargs_repr = [f"{k}={v!r}" for k, v in kwargs.items()]  # 2
-        signature = ", ".join(args_repr + kwargs_repr)           # 3
-        logging.debug(msg=f"Calling {func.__name__}({signature})")
-        value = func(*args, **kwargs)
-        logging.debug(msg=f"{func.__name__!r} returned {value!r}")           # 4
-        return value
-    return wrapper_debug
+import warnings
+from numpy import result_type
 
 
 def timer(func):
     """Print the runtime of the decorated function"""
+
     @functools.wraps(func)
-    def wrapper_timer(*args, **kwargs):
-        start_time = time.perf_counter()    # 1
+    def _wrapper_timer(*args, **kwargs):
+        start_time = time.perf_counter()  # 1
         value = func(*args, **kwargs)
-        end_time = time.perf_counter()      # 2
-        run_time = end_time - start_time    # 3
+        end_time = time.perf_counter()  # 2
+        run_time = end_time - start_time  # 3
         print(f"Finished {func.__name__!r} in {run_time:.4f} secs")
         return value
-    return wrapper_timer
+
+    return _wrapper_timer
+
+
+def suppress_warning(warning):
+    def _warning_decorator(func):
+        @functools.wraps(func)
+        def _wrapper_warning_decorator(*args, **kwargs):
+            with warnings.catch_warnings():
+                warnings.filterwarnings('ignore', category=warning)
+                res = func(*args, **kwargs)
+            return res
+        return _wrapper_warning_decorator
+    return _warning_decorator
